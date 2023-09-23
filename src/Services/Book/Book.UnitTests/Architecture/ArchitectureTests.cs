e73b7531d3fa2248999243879a5dc10268965c2d
Matheus Xavier <mxavier@neowrk.com>
2023-09-23 19:35:12 -0300

    Creating infrastructure layer

--- src/Services/Book/Book.UnitTests/Architecture/ArchitectureTests.cs
+++ src/Services/Book/Book.UnitTests/Architecture/ArchitectureTests.cs
@@ -8,6 +8,7 @@ public class ArchitectureTests
 {
     private const string DomainNamespace = "Book.Domain";
     private const string ApplicationNamespace = "Book.Application";
+    private const string InfrastructureNamespace = "Book.Infrastructure";
 
     [Fact]
     public void BookDomain_Dependency_ShouldNotHaveDepedencyOnOtherProjects()
@@ -17,6 +18,26 @@ public class ArchitectureTests
         string[] otherProjects = new[]
         {
             ApplicationNamespace,
+        };
+
+        // Act
+        TestResult result = Types.InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+
+    [Fact]
+    public void BookApplication_Dependency_ShouldNotHaveDependencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = Assembly.Load(ApplicationNamespace);
+        string[] otherProjects = new[]
+        {
+            InfrastructureNamespace,
         };
 
         // Act
