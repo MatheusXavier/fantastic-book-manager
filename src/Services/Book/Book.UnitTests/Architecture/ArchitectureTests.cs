29b7d78122b474d5588f1fc4a56e8a81c5051037
Matheus Xavier <mxavier@neowrk.com>
2023-09-23 19:24:17 -0300

    Creating application layer

---
+++ src/Services/Book/Book.UnitTests/Architecture/ArchitectureTests.cs
@@ -0,0 +1,31 @@
+﻿using NetArchTest.Rules;
+
+using System.Reflection;
+
+namespace Book.UnitTests.Architecture;
+
+public class ArchitectureTests
+{
+    private const string DomainNamespace = "Book.Domain";
+    private const string ApplicationNamespace = "Book.Application";
+
+    [Fact]
+    public void BookDomain_Dependency_ShouldNotHaveDepedencyOnOtherProjects()
+    {
+        // Arrange
+        var assembly = Assembly.Load(DomainNamespace);
+        string[] otherProjects = new[]
+        {
+            ApplicationNamespace,
+        };
+
+        // Act
+        TestResult result = Types.InAssembly(assembly)
+            .ShouldNot()
+            .HaveDependencyOnAll(otherProjects)
+            .GetResult();
+
+        // Assert
+        result.IsSuccessful.Should().BeTrue();
+    }
+}
