cd0154724e87fe96eae6f2f8afb47423c9687953
Matheus Xavier <mxavier@neowrk.com>
2023-09-23 18:05:09 -0300

    Creating base command class

---
+++ src/Services/Book/Book.Domain/Common/BaseCommand.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation.Results;
+
+using MediatR;
+
+namespace Book.Domain.Common;
+
+
+/// <summary>
+/// By creating a base class that inherits from <see cref="IRequest"/> I can abstract the dependency 
+/// of my entire system to this class instead of a library interface. If at any point we want to 
+/// change the lib, just change this reference here.
+/// </summary>
+public abstract record BaseCommand : IRequest
+{
+    public virtual ValidationResult Validate()
+    {
+        throw new NotImplementedException();
+    }
+}
