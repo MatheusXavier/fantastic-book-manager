79ff02cf94a8fd6cc1adddbc0377340cef87c50c
Matheus Xavier <mxavier@neowrk.com>
2023-09-24 13:16:57 -0300

    Adding docker compose support

--- src/Services/Book/Book.Infrastructure/Database/Migrations/MigrationsManager.cs
+++ src/Services/Book/Book.Infrastructure/Database/Migrations/MigrationsManager.cs
@@ -1,7 +1,11 @@
 ﻿using DbUp;
 using DbUp.Engine;
 
+using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+
+using Polly;
+using Polly.Retry;
 
 using System.Reflection;
 
@@ -11,9 +15,10 @@ public static class MigrationsManager
 {
     public static void MigrateDb(IConfiguration configuration)
     {
-        string? connectionString = configuration.GetConnectionString("BookDb");
+        string? connectionString = configuration.GetConnectionString("BookDb") ??
+            throw new InvalidOperationException("Could not found book db connection string");
 
-        EnsureDatabase.For.SqlDatabase(connectionString);
+        EnsureDatabaseExists(connectionString);
 
         UpgradeEngine upgrader = DeployChanges.To
             .SqlDatabase(connectionString)
@@ -27,5 +32,20 @@ public static class MigrationsManager
         {
             throw new InvalidOperationException("Could not apply migrations to database");
         }
+    }
+
+    private static void EnsureDatabaseExists(string connectionString)
+    {
+        RetryPolicy retry = Policy.Handle<SqlException>()
+            .WaitAndRetry(new TimeSpan[]
+            {
+                TimeSpan.FromSeconds(3),
+                TimeSpan.FromSeconds(5),
+                TimeSpan.FromSeconds(8),
+            });
+
+        //if the sql server container is not created on run docker compose this
+        //migration can't fail for network related exception. 
+        retry.Execute(() => EnsureDatabase.For.SqlDatabase(connectionString));
     }
 }
