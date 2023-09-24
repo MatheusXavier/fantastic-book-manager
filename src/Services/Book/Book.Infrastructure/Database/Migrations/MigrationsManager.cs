3367f4ac3b2ff72425da8abd06d20d6230b5b0c1
Matheus Xavier <mxavier@neowrk.com>
2023-09-24 00:55:51 -0300

    Add DbUp and the first migration to create books table

---
+++ src/Services/Book/Book.Infrastructure/Database/Migrations/MigrationsManager.cs
@@ -0,0 +1,31 @@
+﻿using DbUp;
+using DbUp.Engine;
+
+using Microsoft.Extensions.Configuration;
+
+using System.Reflection;
+
+namespace Book.Infrastructure.Database.Migrations;
+
+public static class MigrationsManager
+{
+    public static void MigrateDb(IConfiguration configuration)
+    {
+        string? connectionString = configuration.GetConnectionString("BookDb");
+
+        EnsureDatabase.For.SqlDatabase(connectionString);
+
+        UpgradeEngine upgrader = DeployChanges.To
+            .SqlDatabase(connectionString)
+            .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+            .LogToAutodetectedLog()
+            .Build();
+
+        DatabaseUpgradeResult result = upgrader.PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            throw new InvalidOperationException("Could not apply migrations to database");
+        }
+    }
+}
